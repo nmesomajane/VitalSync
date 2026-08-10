013a6c5b772a252f3b2490c6bc0999abc3f8a1ce
nmeso <nnopujane@gmail.com>
2026-08-10 23:46:11 +0100

    refactor: remove unused SecureStore import from useSocket hook

--- client/hooks/useSocket.ts
+++ client/hooks/useSocket.ts
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react";
 import { io, Socket } from "socket.io-client";
-import * as SecureStore from "expo-secure-store";
+
 import useVitalsStore from "../src/store/vitalsStore";
 import useAuthStore from "../src/store/authStore";
 import { Vitals, Alert } from "../src/types";
